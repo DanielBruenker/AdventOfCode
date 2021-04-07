592c9f61345c050f846ee6146a522e9c02a6c91a
Daniel Bruenker <Daniel.Bruenker@gmx.de>
2021-04-07 17:40:37 +0200

    Add JUnit test for firstPart() of day2

--- src/test/java/org/haffson/adventofcode/days/day02/Day02Test.java
+++ src/test/java/org/haffson/adventofcode/days/day02/Day02Test.java
@@ -1,11 +1,19 @@
 package org.haffson.adventofcode.days.day02;
 
-import org.haffson.adventofcode.days.day02.Day02;
 import org.haffson.adventofcode.utils.FileReaders;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 @SpringBootTest
@@ -21,4 +29,25 @@ public class Day02Test {
         int actualResult = day02.getDay();
         Assert.assertEquals(expectedResult, actualResult);
     }
+
+    @Test
+    public void testFirstPartReturnsExpectedResult() {
+        FileReaders fileReadersMock = mock(FileReaders.class);
+        List<String> stringList = Arrays.asList(
+                "1-3 a: abcde",
+                "1-3 b: cdefg",
+                "2-9 c: ccccccccc"
+        );
+        String expectedResult = "Part 1: " + 2;
+
+        when(fileReadersMock.readFileIntoStringList(anyString()))
+                .thenReturn(stringList);
+
+        Day02 day02 = new Day02(fileReadersMock, anyString());
+
+        String actualResult = day02.firstPart();
+
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
+
 }
