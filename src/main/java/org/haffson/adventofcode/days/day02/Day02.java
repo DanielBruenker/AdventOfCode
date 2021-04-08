a62e23a7808b2152fae84453ef6da62710a8dbb5
Daniel Bruenker <Daniel.Bruenker@gmx.de>
2021-04-08 09:20:22 +0200

    Add PasswordPolice class and implement calculatePart1 methode

--- src/main/java/org/haffson/adventofcode/days/day02/Day02.java
+++ src/main/java/org/haffson/adventofcode/days/day02/Day02.java
@@ -6,6 +6,7 @@ import org.haffson.adventofcode.utils.FileReaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.List;
@@ -35,7 +36,7 @@ public class Day02 implements Days {
     @Autowired
     public Day02(FileReaders fileReaders, @Value("{$day2.file}") String filePath) {
         this.problemStatus = new HashMap<>();
-        this.problemStatus.put("1", ProblemStatusEnum.UNSOLVED);
+        this.problemStatus.put("1", ProblemStatusEnum.SOLVED);
         this.problemStatus.put("2", ProblemStatusEnum.UNSOLVED);
         this.fileReaders = fileReaders;
         this.filePath = filePath;
@@ -71,18 +72,56 @@ public class Day02 implements Days {
         return null;
     }
 
-    private int calculatePart1(){
-       for(String passwordAndPolice : passwordWithPoliceList){
-           break;
-           //TODO
-           // 1. Split police from password
-           // 2. Parse minOccurrence, maxOccurrence and the letter
-           // 3. Count the occurrence of the letter of the password police
-           // 4. Check if the password valid, by compare the occurrence with minOccurrence and maxOccurrence
-           // Split code in small methods and test these with Unit tests
-       }
+    private int calculatePart1() {
+        int validPasswordCount = 0;
+        for (String passwordAndPolice : passwordWithPoliceList) {
+            // Parse password and police
+            String policeStr = passwordAndPolice.substring(0, 5);
+            String password = passwordAndPolice.substring(7);
 
-       return 2;
+            PasswordPolice police = PasswordPolice.fromString(policeStr);
+
+            if (police.validatePassword(password)) {
+                validPasswordCount++;
+            }
+        }
+        return validPasswordCount;
+    }
+}
+
+
+class PasswordPolice {
+
+    private final int minOccurrence;
+    private final int macOccurrence;
+    private final char character;
+
+    public PasswordPolice(char character, int minOccurrence, int macOccurrence) {
+        this.character = character;
+        this.minOccurrence = minOccurrence;
+        this.macOccurrence = macOccurrence;
+
+    }
+
+    static PasswordPolice fromString(String policeStr) {
+        if (!PasswordPolice.isPasswordPoliceStringValid(policeStr)) {
+            throw new IllegalArgumentException("Password police string ist not valid!");
+        }
+
+        char character = policeStr.charAt(4);
+        int minOccurrence = Character.getNumericValue(policeStr.charAt(0));
+        int maxOccurrence = Character.getNumericValue(policeStr.charAt(2));
+
+        return new PasswordPolice(character, minOccurrence, maxOccurrence);
+    }
+
+    static boolean isPasswordPoliceStringValid(String policeStr) {
+        return policeStr.matches("[\\d][-][\\d][\\s][a-z]");
+    }
+
+    public boolean validatePassword(String password) {
+        int occurrence = StringUtils.countOccurrencesOf(password, this.character + "");
+        return minOccurrence <= occurrence && occurrence <= macOccurrence;
     }
 
 }
