19607ac0f8e7d68ae62b2a7625e0d29a66e2ea83
Daniel Bruenker <Daniel.Bruenker@gmx.de>
2021-04-09 17:51:16 +0200

    Document the Slope class

--- src/main/java/org/haffson/adventofcode/days/day03/Slope.java
+++ src/main/java/org/haffson/adventofcode/days/day03/Slope.java
@@ -1,9 +1,13 @@
 package org.haffson.adventofcode.days.day03;
 
+/**
+ * The class is used in Day03 to declare different slopes and pass these to
+ * the crossAndCountTrees methode of the Forest class.
+ */
 class Slope {
 
-    private final Integer down;
-    private final Integer right;
+    private final Integer down; //Steps down
+    private final Integer right; //Steps to the right
 
     public Slope(Integer right, Integer down) {
         this.down = down;
