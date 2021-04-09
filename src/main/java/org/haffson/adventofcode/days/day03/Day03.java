c38322b28dfd790a95b87909d959a1a5c8e994f8
Daniel Bruenker <Daniel.Bruenker@gmx.de>
2021-04-09 13:43:43 +0200

    Add implementation for Day03 Part1

--- src/main/java/org/haffson/adventofcode/days/day03/Day03.java
+++ src/main/java/org/haffson/adventofcode/days/day03/Day03.java
@@ -2,9 +2,13 @@ package org.haffson.adventofcode.days.day03;
 
 import org.haffson.adventofcode.ProblemStatusEnum;
 import org.haffson.adventofcode.days.Days;
+import org.haffson.adventofcode.utils.FileReaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.List;
 
 
 /**
@@ -16,16 +20,38 @@ public class Day03 implements Days {
     /** The puzzle status {@code HashMap} */
     private final HashMap<String, ProblemStatusEnum> problemStatus;
 
-    public Day03(){
+    private List<String> rows;
+
+    private FileReaders fileReaders;
+
+    private String filePath;
+
+    Day03(){
         this.problemStatus = new HashMap<>();
-        this.problemStatus.put("1", ProblemStatusEnum.UNSOLVED);
+        this.problemStatus.put("1", ProblemStatusEnum.SOLVED);
         this.problemStatus.put("2", ProblemStatusEnum.UNSOLVED);
     }
 
+    @Autowired
+    Day03(FileReaders fileReaders, @Value("$(day3.file)") String filePath){
+        this();
+        this.fileReaders = fileReaders;
+        this.filePath = filePath;
+    }
+
+    /**
+     * This Method is used to load the puzzle inputs before execute on of the puzzle parts.
+     */
+    private void loadPuzzleInputs() {
+        if (rows == null) {
+            rows = fileReaders.readFileIntoStringList(filePath);
+        }
+    }
 
     @Override
     public String firstPart() {
-        return null;
+        loadPuzzleInputs();
+        return "Part 1: " + calculatePart1();
     }
 
     @Override
@@ -35,12 +61,17 @@ public class Day03 implements Days {
 
     @Override
     public int getDay() {
-        return 0;
+        return 3;
     }
 
     @Override
     public HashMap<String, ProblemStatusEnum> getProblemStatus() {
         return problemStatus;
+    }
+
+    private int calculatePart1(){
+        Forest forest = Forest.fromMapStringList(rows);
+        return forest.crossAndCountTrees(3, 1);
     }
 
 }
--- src/test/java/org/haffson/adventofcode/days/day03/Day03Test.java
+++ src/test/java/org/haffson/adventofcode/days/day03/Day03Test.java
@@ -1,12 +1,59 @@
 package org.haffson.adventofcode.days.day03;
 
+import org.haffson.adventofcode.utils.FileReaders;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 @SpringBootTest
 public class Day03Test {
 
+    @Autowired
+    private FileReaders fileReaders;
+
     @Test
-    public void testFirstPartReturnsExpectedResult(){}
+    public void testGetDay() {
+        Day03 day03 = new Day03();
+        int expectedResult = 3;
+        int actualResult = day03.getDay();
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void testFirstPartReturnsExpectedResult(){
+        FileReaders fileReadersMock = mock(FileReaders.class);
+        List<String> rows = Arrays.asList(
+                ".#...#.......#...#...#.#.#.....",
+                "####.....#.#..#...#...........#",
+                ".....#...........#......#....#.",
+                "......#..#......#.#..#...##.#.#",
+                "............#......#...........",
+                "...........#.#.#....#.......##.",
+                "....#.......#..............#...",
+                "........##...#.#.....##...##.#.",
+                ".#.#.....##................##..",
+                ".##................##..#...##.."
+        );
+
+        String expectedResult = "Part 1: " + 2;
+
+        when(fileReadersMock.readFileIntoStringList(anyString()))
+                .thenReturn(rows);
+
+        Day03 day03 = new Day03(fileReadersMock, anyString());
+
+        String actualResult = day03.firstPart();
+
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
 
 }
